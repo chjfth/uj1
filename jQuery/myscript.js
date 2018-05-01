8d4a6443410129a49050fd37cd1de7da4c9a1b29
chj <chjfth@gmail.com>
2018-05-01 18:38:24 +0800

    // Click "Submit" to fade out all content. $('input').eq(1).click(function(){   $('.container').fadeOut(1000) 	console.log(this); })

--- jQuery/myscript.js
+++ jQuery/myscript.js
@@ -2,3 +2,9 @@
 $('h1').click(function(){
 	$(this).text("I was changed!")
 })
+
+// Click "Submit" to fade out all content.
+$('input').eq(1).click(function(){
+  $('.container').fadeOut(1000)  
+	console.log(this);
+})
